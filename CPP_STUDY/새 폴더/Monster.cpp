15be862d5aa1ec14b769611d275ce6583dbdc069
Hoshi03 <81688152+Hoshi03@users.noreply.github.com>
2022-06-03 14:42:55 +0900

    Delete CPP_STUDY/새 폴더 directory

--- CPP_STUDY/새 폴더/Monster.cpp
+++
@@ -1,9 +0,0 @@
-#include "Monster.h"
-#include <iostream>
-using namespace std;
-
-void Monster::printinfo() {
-	cout << "-----------------------------" << endl;
-	cout << "[몬스터 정보] " << "HP: " << _hp << " ATK: " << _attack << " DEF: " << _defence << endl;;
-	cout << "-----------------------------" << endl;
-}
